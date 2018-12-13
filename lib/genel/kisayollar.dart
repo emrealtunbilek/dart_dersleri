e6c7e8acaac09742ff6d376b49628a0271c246a5
Emre Altunbilek <emrealtunbilek@gmail.com>
2018-12-13 21:30:41 +0300

    initial commit

---
+++ lib/genel/kisayollar.dart
@@ -0,0 +1,28 @@
+main(List<String> args) {
+// CTRL + X keser, CTRL + C kopyalar, CTRL + V yapıştırır.
+// CTRL  + Z geri alır, CTRL + shıft + Z ileri sarar.
+// CTRL + C tüm satırı kopyalar.
+// CTRL + ok tuşları kelime kelime ilerlemeyi sağlar.
+
+// CTRL + SHIFT + oklar kelime kelime seçim yaptırır.
+// ALT + yukarı/asagı okları ile ilgili satır veya seçili alan taşınabilir.
+// ALT + click ile birden fazla imlece sahip olunabilir.
+// CTRL + K + C seçili alanı yorum satırı yapar
+// CTRL + ö satırı yorum satırı yapar
+// SHIFT + ALT + A seçili blogu yorum satırı yapar
+// SHIFT + ALT + yukarı asagı okları satırı kopyalar
+    
+// CTRL + P dosya arar
+// CTRL + F Dosyanın içinde arama yapar
+// CTRL + ALT +F bul ve değiştir
+// CTRL + SHIFT + F tüm dosyalarda arar.
+
+// CTRL + B Sol menuyu açar veya kapatır
+// CTRL + tab bir sonraki sekme, CTRL + SHIFT + tab bir önceki sekme
+// CTRL + K + F seçili alanı, ALT + SHIFT  + F tüm dosyayı formatlı gösterir
+// CTRL + " terminali açar kapar 
+
+
+
+
+}
